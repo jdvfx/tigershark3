542faf620ab245fb4b8504d1d5808d778a877911
Julien Depredurand <julien.depredurand@gmail.com>
2022-10-03 00:08:41 -0700

    unwrap to empty strings if try_get() fails, can check with is_empty()

--- src/assetdef.rs
+++ src/assetdef.rs
@@ -26,13 +26,13 @@ impl From<&SqliteRow> for Version {
             asset_id: row.try_get("asset_id").unwrap_or(0),
             version_id: row.try_get("asset_id").unwrap_or(0),
             version: row.try_get("version").unwrap_or(0_i64),
-            source: row.try_get("source").unwrap_or("_".to_string()),
-            datapath: row.try_get("datapath").unwrap_or("_".to_string()),
-            depend: row.try_get("depend").unwrap_or("_".to_string()),
+            source: row.try_get("source").unwrap_or("".to_string()),
+            datapath: row.try_get("datapath").unwrap_or("".to_string()),
+            depend: row.try_get("depend").unwrap_or("".to_string()),
             approved: row.try_get("approved").unwrap_or(0),
             status: row.try_get("status").unwrap_or(0),
-            ctime: row.try_get("ctime").unwrap_or("_".to_string()),
-            atime: row.try_get("atime").unwrap_or("_".to_string()),
+            ctime: row.try_get("ctime").unwrap_or("".to_string()),
+            atime: row.try_get("atime").unwrap_or("".to_string()),
         }
     }
 }
@@ -42,13 +42,13 @@ impl Into<Version> for SqliteRow {
             asset_id: self.try_get("asset_id").unwrap_or(0),
             version_id: self.try_get("asset_id").unwrap_or(0),
             version: self.try_get("version").unwrap_or(0_i64),
-            source: self.try_get("source").unwrap_or("_".to_string()),
-            datapath: self.try_get("datapath").unwrap_or("_".to_string()),
-            depend: self.try_get("depend").unwrap_or("_".to_string()),
+            source: self.try_get("source").unwrap_or("".to_string()),
+            datapath: self.try_get("datapath").unwrap_or("".to_string()),
+            depend: self.try_get("depend").unwrap_or("".to_string()),
             approved: self.try_get("approved").unwrap_or(0),
             status: self.try_get("status").unwrap_or(0),
-            ctime: self.try_get("ctime").unwrap_or("_".to_string()),
-            atime: self.try_get("atime").unwrap_or("_".to_string()),
+            ctime: self.try_get("ctime").unwrap_or("".to_string()),
+            atime: self.try_get("atime").unwrap_or("".to_string()),
         }
     }
 }
@@ -63,8 +63,8 @@ impl From<&SqliteRow> for Asset {
     fn from(row: &SqliteRow) -> Asset {
         Asset {
             asset_id: row.try_get("asset_id").unwrap_or(0_i64),
-            name: row.try_get("name").unwrap_or("_".to_string()),
-            location: row.try_get("location").unwrap_or("_".to_string()),
+            name: row.try_get("name").unwrap_or("".to_string()),
+            location: row.try_get("location").unwrap_or("".to_string()),
         }
     }
 }
@@ -72,8 +72,8 @@ impl Into<Asset> for SqliteRow {
     fn into(self) -> Asset {
         Asset {
             asset_id: self.try_get("asset_id").unwrap_or(0_i64),
-            name: self.try_get("name").unwrap_or("_".to_string()),
-            location: self.try_get("location").unwrap_or("_".to_string()),
+            name: self.try_get("name").unwrap_or("".to_string()),
+            location: self.try_get("location").unwrap_or("".to_string()),
         }
     }
 }
