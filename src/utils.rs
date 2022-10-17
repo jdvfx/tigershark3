5eb195db829eae98bc2162266d81c68c2caf7f9d
Julien Depredurand <julien.depredurand@gmail.com>
2022-10-16 18:36:01 -0700

    added node about returning id

--- src/utils.rs
+++ src/utils.rs
@@ -115,6 +115,9 @@ pub async fn create_version(mut connection: PoolConnection<Sqlite>, json: AssetJ
         ct = now(),
     );
 
+    // should be able to add RETURNING asset_id   after VALUES ()
+    //
+
     let sql = sqlx::query(&q).execute(&mut connection).await;
     match sql {
         Ok(_) => return CliOutput::new("ok", &format!("{new_version}")),
