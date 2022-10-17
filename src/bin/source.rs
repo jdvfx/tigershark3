320bc37998a5fef3b6e9d7ba5afa1fd25c0c7ded
jdvfx <julien.depredurand@gmail.com>
2022-10-16 20:16:59 -0700

    import parent assetdef.rs

--- src/bin/source.rs
+++ src/bin/source.rs
@@ -1,4 +1,8 @@
+#[path="../assetdef.rs"]
+mod assetdef;
+
 fn main(){
+    // assetdef::Asset    
     println!("source path_to_a_file.bgeo.sc ");
     println!("will return the .hip or .hipnc file that created it");
 
