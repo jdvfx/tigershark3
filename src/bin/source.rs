b57e62942ec5f0189292743836132b23190ab29b
Julien Depredurand <julien.depredurand@gmail.com>
2022-10-27 22:21:20 -0700

    removed bin/source.rs, will be replace by internal command without asset arg

--- src/bin/source.rs
+++
@@ -1,8 +0,0 @@
-#[path = "../assetdef.rs"]
-mod assetdef;
-
-fn main() {
-    // assetdef::Asset
-    println!("source path_to_a_file.bgeo.sc ");
-    println!("will return the .hip or .hipnc file that created it");
-}
