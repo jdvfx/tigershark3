72ce481ccb0578b7416480b0a35484e75e50d214
jdvfx <ju.dep@zoho.com>
2023-02-27 22:49:54 -0800

    use Default value for structs

--- src/assetdef.rs
+++ src/assetdef.rs
@@ -7,7 +7,7 @@ use sqlx::Row;
 // 2 : purge
 // 3 : deleted
 
-#[derive(sqlx::FromRow, Debug, Clone)]
+#[derive(sqlx::FromRow, Debug, Clone, Default)]
 pub struct Version {
     pub asset_id: i64,
     pub version_id: i64,
@@ -24,22 +24,22 @@ pub struct Version {
 impl From<&SqliteRow> for Version {
     fn from(row: &SqliteRow) -> Version {
         Version {
-            asset_id: row.try_get("asset_id").unwrap_or(0),
-            version_id: row.try_get("asset_id").unwrap_or(0),
-            version: row.try_get("version").unwrap_or(0_i64),
-            source: row.try_get("source").unwrap_or_else(|_| "".to_string()),
-            datapath: row.try_get("datapath").unwrap_or_else(|_| "".to_string()),
-            depend: row.try_get("depend").unwrap_or_else(|_| "".to_string()),
-            approved: row.try_get("approved").unwrap_or(0),
-            status: row.try_get("status").unwrap_or(0),
-            ctime: row.try_get("ctime").unwrap_or_else(|_| "".to_string()),
-            atime: row.try_get("atime").unwrap_or_else(|_| "".to_string()),
+            asset_id: row.try_get("asset_id").unwrap_or_default(),
+            version_id: row.try_get("asset_id").unwrap_or_default(),
+            version: row.try_get("version").unwrap_or_default(),
+            source: row.try_get("source").unwrap_or_default(),
+            datapath: row.try_get("datapath").unwrap_or_default(),
+            depend: row.try_get("depend").unwrap_or_default(),
+            approved: row.try_get("approved").unwrap_or_default(),
+            status: row.try_get("status").unwrap_or_default(),
+            ctime: row.try_get("ctime").unwrap_or_default(),
+            atime: row.try_get("atime").unwrap_or_default(),
         }
     }
 }
 
 // --------------------------------------------
-#[derive(sqlx::FromRow, Debug, Clone)]
+#[derive(sqlx::FromRow, Debug, Clone, Default)]
 pub struct Asset {
     pub asset_id: i64,
     pub name: String,
@@ -49,9 +49,9 @@ pub struct Asset {
 impl From<SqliteRow> for Asset {
     fn from(val: SqliteRow) -> Self {
         Asset {
-            asset_id: val.try_get("asset_id").unwrap_or(0_i64),
-            name: val.try_get("name").unwrap_or_else(|_| "".to_string()),
-            location: val.try_get("location").unwrap_or_else(|_| "".to_string()),
+            asset_id: val.try_get("asset_id").unwrap_or_default(),
+            name: val.try_get("name").unwrap_or_default(),
+            location: val.try_get("location").unwrap_or_default(),
         }
     }
 }
