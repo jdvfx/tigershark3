bac3603b9ede5b24572357fdca2a701a7c2ea240
Julien Depredurand <julien.depredurand@gmail.com>
2023-08-15 22:57:41 -0700

    cleanup

--- setup.sh
+++ setup.sh
@@ -45,9 +45,8 @@ mkdir -p $TS_PATH
 mkdir -p $DB_PATH
 cp -r * $TS_PATH
 echo $TS_PATH > ~/.tigershark_db_path
-# sqlite3 $DB_PATH/$DB_NAME ".read db/schema.sql"
+sqlite3 $DB_PATH/$DB_NAME ".read db/schema.sql"
 
 echo install config saved in: ~/.tigershark_db_path
 echo DB created: $DB_PATH/$DB_NAME
-
 echo DATABASE_URL=sqlite:$DB_PATH/$DB_NAME > .env
