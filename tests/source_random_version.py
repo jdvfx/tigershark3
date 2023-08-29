dd73748c30b48be8fbcbc73af5723243c7d6eae7
jdvfx <ju.dep@zoho.com>
2023-08-28 21:15:20 -0700

    test: source_random_version

---
+++ tests/source_random_version.py
@@ -0,0 +1,60 @@
+import os
+import random
+import json
+from typing import Optional
+from subprocess import Popen, PIPE
+
+thisdir = os.path.dirname(__file__)
+ts_exe = f"{thisdir}/../target/debug/tigershark3"
+
+def get_random_asset() -> Optional[str]:
+    db = os.environ['TS_DATABASE_URL']
+    command = f"sqlite3 {db}"
+    c = command.split(" ")
+    x = 'SELECT version_id FROM versions'
+    c.append(x)
+    lines = []
+    try:
+        process = Popen(c, stdout=PIPE)
+        (output, _err) = process.communicate()
+        _exit_code = process.wait()
+        output = output.decode("utf-8")
+        lines = output.splitlines()
+    except:
+        return None
+
+    if len(lines)>0:
+        return random.choice(lines) 
+    return None
+
+
+def build_asset(asset_string:str) -> dict:
+    r = asset_string
+    asset = {
+            "version_id":int(r),
+            }
+    return asset
+
+def ts(command,asset):
+    try:
+        process = Popen([ts_exe,"-c",command,"-a",json.dumps(asset)], stdout=PIPE)
+        (output, _err) = process.communicate()
+        exit_code = process.wait()
+        output = output.decode("utf-8")
+        if exit_code == 0:
+            return (0,output)
+        else:
+            return (1,output)
+    except:
+        return (1,"Python Popen failed")
+#
+#
+
+command = "source"
+random_asset:Optional[str]= get_random_asset()
+if random_asset != None:
+    asset = build_asset(random_asset)
+    clioutput = ts(command,asset)
+    print(command,asset)
+    print(clioutput)
+
