1d11be42dc18fd6587e9aef992fb7141ae6f13af
Julien Depredurand <julien.depredurand@gmail.com>
2023-10-07 15:44:50 -0700

    checks if ts_database_url env is set

--- sqldump_versions.sh
+++ sqldump_versions.sh
@@ -1,1 +1,7 @@
-sqlite3 $TS_DATABASE_URL "SELECT * FROM versions"
+if [ x"${TS_DATABASE_URL}" == "x" ]; then
+	echo TS_DATABASE_URL environment variable not set
+else 
+	echo TS_DATABASE_URL: $TS_DATABASE_URL 
+	sqlite3 $TS_DATABASE_URL "SELECT * FROM versions"
+fi
+
