23511d2dfe9933fe254aee89b2295e74495f04c4
Julien Depredurand <julien.depredurand@gmail.com>
2023-10-07 23:50:03 -0700

    fix pub

--- src/frame_num.rs
+++ src/frame_num.rs
@@ -1,7 +1,7 @@
 // replace frame number with hash (#) chars
 // "path/file.1234.ext" -> "path/file.####.ext"
 
-fn find_replace_frame_num(filepath: &str) -> String {
+pub fn find_replace_frame_num(filepath: &str) -> String {
     let f: Vec<_> = filepath
         .split(".")
         .map(|c| {
