12c2ecbcd277f863dfb5de15963e738f2e693a22
Julien Depredurand <julien.depredurand@gmail.com>
2023-10-09 23:38:49 -0700

    updated frame_num test, no longer a result, always outputs a string

--- tests/test_frame_nums.rs
+++ tests/test_frame_nums.rs
@@ -6,26 +6,26 @@ use tigershark3::frame_num::find_replace_frame_num;
 fn two_numbers_b() {
     let x = "/my/weird/path/test34.12.1234.bgeo.sc";
     let r = "/my/weird/path/test34.##.####.bgeo.sc";
-    assert_eq!(find_replace_frame_num(x).unwrap_or("".to_string()), r);
+    assert_eq!(find_replace_frame_num(x), r);
 }
 
 #[test]
 fn missing_dot() {
     let x = "test.34exr";
     let r = "test.34exr";
-    assert_eq!(find_replace_frame_num(x).unwrap_or("".to_string()), r);
+    assert_eq!(find_replace_frame_num(x), r);
 }
 
 #[test]
 fn noslash() {
     let x = "test.34.exr";
     let r = "test.##.exr";
-    assert_eq!(find_replace_frame_num(x).unwrap_or("".to_string()), r);
+    assert_eq!(find_replace_frame_num(x), r);
 }
 
 #[test]
 fn test_regular_path() {
     let x = "/bob/joe/test.34.exr";
     let r = "/bob/joe/test.##.exr";
-    assert_eq!(find_replace_frame_num(x).unwrap_or("".to_string()), r);
+    assert_eq!(find_replace_frame_num(x), r);
 }
